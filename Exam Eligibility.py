8b8a124a01d64a8885d2ce891366172b80ba6109
Shivam869 <sanjivshivam23@gmail.com>
2024-10-20 20:27:29 +0530

    Add files via upload

---
+++ Exam Eligibility.py
@@ -0,0 +1,7 @@
+z=all(input("Enter your name:"))
+y=int(input("Enter your roll number:"))
+x=int(input("Enter your age:"))
+if(x>=18):
+    print("Submitted")
+else:
+    print("Must be 18 or older")
